63ab812c90e601cdf76306e47c409e73697c7701
Alexander <alex@alexvoiceover.com>
2023-09-10 18:49:40 +0100

    Added Roulette

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,11 +1,31 @@
-function validateInput() {
-    const inputElement = document.getElementById("numColorsTextbox");
-    const value = parseFloat(inputElement.value);
+function createSegments() {
+    const numColorsTextbox = document.getElementById("numColorsTextbox");
+    const numSegments = parseInt(numColorsTextbox.value);
 
-    if (isNaN(value) || value < 2 || value > 6) {
+    if (isNaN(numSegments) || numSegments < 2 || numSegments > 6) {
         alert("Please enter a number between 2 and 6.");
-        inputElement.value = "3"; // Set default value to 3
+        numColorsTextbox.value = "3"; // Set default value to 3
     } else {
-        document.getElementById("rouletteCircle").setAttribute("data-segments", value);
+        const rouletteCircle = document.getElementById("rouletteCircle");
+        rouletteCircle.innerHTML = "";
+
+        for (let i = 0; i < numSegments; i++) {
+            const segment = document.createElement("div");
+            segment.className = "segment";
+            segment.style.background = getRandomColor();
+            rouletteCircle.appendChild(segment);
+        }
     }
 }
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Initial creation of segments
+createSegments();
